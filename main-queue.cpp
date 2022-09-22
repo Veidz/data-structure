786e4b18b33bac9570a3cc26fe324802f0f1b839
Veidz <joaovictorveidz@gmail.com>
2022-09-22 15:26:36 -0300

    feat: add queue menu implementation

---
+++ main-queue.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "doubly-linked-list.h"
+using namespace std;
+
+int main() {
+  DLL* queue = new DLL();
+  int choice, value;
+
+  do {
+    cout << "\n0 - Exit" << endl;
+    cout << "1 - Enqueue" << endl;
+    cout << "2 - Dequeue" << endl;
+    cout << "3 - Print Queue" << endl;
+    cin >> choice;
+
+    switch (choice) {
+      case 0:
+        exit(1);
+        break;
+      case 1:
+        cout << "Enter a value to enqueue: ";
+        cin >> value;
+        queue->insertBeginning(value);
+        break;
+      case 2:
+        queue->removeNode();
+        break;
+      case 3:
+        queue->print();
+        break;
+    }
+
+  } while (choice != 0);
+
+  return 0;
+
+  system("pause");
+}
