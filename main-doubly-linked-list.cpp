fe5bd967446d438d5a9287647c802c0faad9021e
Veidz <joaovictorveidz@gmail.com>
2022-09-22 15:13:36 -0300

    feat: add doubly linked list main menu file

---
+++ main-doubly-linked-list.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "doubly-linked-list.h"
+using namespace std;
+
+int main() {
+  DLL* list = new DLL();
+  int choice, value;
+
+  do {
+    cout << "\n0 - Exit" << endl;
+    cout << "1 - Insert Beginning" << endl;
+    cout << "2 - Insert End" << endl;
+    cout << "3 - Remove" << endl;
+    cout << "4 - Print List" << endl;
+    cin >> choice;
+
+    switch (choice) {
+      case 0:
+        exit(1);
+        break;
+      case 1:
+        cout << "Enter a value to insert at the beginning: ";
+        cin >> value;
+        list->insertBeginning(value);
+        break;
+      case 2:
+        cout << "Enter a value to insert at the end: ";
+        cin >> value;
+        list->insertEnd(value);
+        break;
+      case 3:
+        list->removeNode();
+        break;
+      case 4:
+        list->print();
+        break;
+    }
+
+  } while (choice != 0);
+
+  return 0;
+
+  system("pause");
+}
